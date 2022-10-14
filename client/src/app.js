5bd0bf6252862389aad3a725dac182104df1d289
Prashant Bhimrathi <prashantignitor7@gmail.com>
2022-10-15 01:08:22 +0530

    Second commit

--- client/src/app.js
+++ client/src/app.js
@@ -1,38 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
-import { Grid, CircularProgress } from '@material-ui/core'
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import { Grid, CircularProgress } from "@material-ui/core";
+import "./app.css";
 
 const App = () => {
-   const [data, setData] =useState([])
-   useEffect(()=>{
-       const socket = socketIOClient("http://localhost:4001")
-       socket.on('data1',(data)=>{
-           setData(data)
-       })
-   },[])
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const socket = socketIOClient("http://localhost:4001");
+    socket.on("data1", (data) => {
+      setData(data);
+    });
+  }, []);
 
-   
-
-   return (
-    !data.length ? <CircularProgress /> : (
-      <div>
-        <Grid container alignItems="stretch" spacing={3}>
-        {data.map((dat) => (
-          <Grid key={dat._id} item xs={12} sm={4} md={4}>
-            {dat}
+  return (
+    <>
+      <h1> Data Acquisition Using Scoket.io</h1>
+      {!data.length ? (
+        <CircularProgress />
+      ) : (
+        <div>
+          <Grid container alignItems="stretch" spacing={3}>
+            {data.map((dat) => (
+              <Grid
+                key={dat._id}
+                item
+                xs={12}
+                sm={4}
+                md={4}
+                className="grid-item"
+              >
+                {dat}
+              </Grid>
+            ))}
           </Grid>
-        ))}
-        </Grid>
+          <br></br>
 
-        <br></br>
-
-        <button onClick={ ()=>{ setData(Array.from({length: 9}, ()=> 1000))}}> Reset</button>
-        
-      </div>
-      
-   )
-   )
-  
+          <button
+            onClick={() => {
+              setData(Array.from({ length: 9 }, () => 1000));
+            }}
+          >
+            Reset
+          </button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default App;
