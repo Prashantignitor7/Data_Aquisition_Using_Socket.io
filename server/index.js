15a7bd6a0f8354cf89a8062340bbc82b9420dc10
Prashant Bhimrathi <prashantignitor7@gmail.com>
2022-10-15 01:15:44 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -13,11 +13,11 @@ const server= new socketio.Server(httpServer, {
     }
 })
 
-let timeChange 
+
 
 server.on("connection", (socket)=>{
     console.log("connected")
-    if(timeChange) clearInterval(timeChange)
+    
     setInterval(()=>socket.emit("data1", Array.from({length: 9}, () => Math.floor(Math.random() * 4000)+ 1000)),1000)
 
 })
